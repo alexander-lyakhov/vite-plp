cc2133278a3941107be86d110e094068c5313672
alexander-lyakhov <alexr.lyakhov@gmail.com>
2026-02-21 17:13:55 +0200

    ...

--- src/components/plp-grid.vue
+++ src/components/plp-grid.vue
@@ -19,30 +19,13 @@
 /* @@@ @SCSS@ */
 
 .grid {
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
   justify-content: center;
   padding-top: 80px;
 
   .grid-cell {
     min-width: 20%;
-  }
-}
-@media screen and (max-width: 1600px) {
-  .grid .grid-cell {
-    min-width: 25%;
-  }
-}
-
-@media screen and (max-width: 1280px) {
-  .grid .grid-cell {
-    min-width: 33.33%;
-  }
-}
-
-@media screen and (max-width: 930px) {
-  .grid .grid-cell {
-    min-width: 50%;
   }
 }
 </style>
